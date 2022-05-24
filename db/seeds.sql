750636b5fbbde481f0a56161fa32acd7a291ebc4
Tori <torihendricks927@yahoo.com>
2022-05-24 18:31:51 -0400

    add all values for each table

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO department (id, department_name)
+VALUES (001, "Georgia Retina");
+       
+INSERT INTO role (id, title, salary, department_id)
+VALUES (10, "Technician", 50000, 001),
+       (20, "Physician", 100000, 001),
+       (30, "Manager", 80000, 001);
+       
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Johnny", "Depp", 10, NULL),
+       (2, "Amber", "Heard", 20, NULL),
+       (3, "Jason", "Momoa", 30, 123);
+       
