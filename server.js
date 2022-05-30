24c7a6fd6e456432ee0878893d957210cd8a7f91
Tori <torihendricks927@yahoo.com>
2022-05-30 19:55:59 -0400

    fixed issue with adding new employee and having no manager ID added

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 //  const express = require('express');
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const table = require('console.table');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const table = require("console.table");
 
 // const app = express();
 // const PORT = process.env.PORT || 3001;
-
 
 // Express middleware
 // app.use(express.urlencoded({ extended: false }));
@@ -14,170 +13,191 @@ const table = require('console.table');
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: 'localhost',
+    host: "localhost",
     // port: 3001,
     // MySQL username,
-    user: 'root',
+    user: "root",
     // MySQL password
-    password: 'Dobetter1!',
-    database: 'tracker_db',
+    password: "Dobetter1!",
+    database: "tracker_db",
   },
   console.log(`Connected to the tracker_db database.`)
 );
 
 // connecting to mysql
 db.connect((err) => {
-    if(err){
-        throw err;
-   }
-    console.log("connected");
+  if (err) {
+    throw err;
+  }
+  console.log("connected");
 });
 // Beginning prompt to show main menu
 const mainMenu = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         name: "menu",
         type: "list",
-        message: "Welcome to the Employee Tracker, please select from the following options:",
+        message:
+          "Welcome to the Employee Tracker, please select from the following options:",
         choices: [
-            "View Departments",
-            "View Employees",
-            "View Roles",
-            "Add an employee",,
-            "Add a role",
-            "Add a department",
-            "Update an employee role",
-            "Exit",
-        ]
-    },
-]).then((response) => {
-    console.log(response)
-    // switched response connection to name "menu" instead of choices
-        if (response.menu === "View Departments") {
-            viewDepartments();
-        } else if (response.menu === "View Employees") {
-            viewEmployees();
-        } else if (response.menu === "View Roles") {
-            viewRoles(); 
-        } else if (response.menu === "Add an employee") {
-            addNewEmployee(); 
-        } else if (response.menu === "Add a role") {
-            addNewRole(); 
-        } else if (response.menu === "Add a department") {
-            addNewDepartment(); 
-        } else if (response.menu === "Update an employee role") {
-            employeeRoleUpdate(); 
-        } else {
-            // db.end();
-            console.log("Thank you for choosing our service");
-        }
-})
+          "View Departments",
+          "View Employees",
+          "View Roles",
+          "Add an employee",
+          ,
+          "Add a role",
+          "Add a department",
+          "Update an employee role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      // switched response connection to name "menu" instead of choices
+      if (response.menu === "View Departments") {
+        viewDepartments();
+      } else if (response.menu === "View Employees") {
+        viewEmployees();
+      } else if (response.menu === "View Roles") {
+        viewRoles();
+      } else if (response.menu === "Add an employee") {
+        addNewEmployee();
+      } else if (response.menu === "Add a role") {
+        addNewRole();
+      } else if (response.menu === "Add a department") {
+        addNewDepartment();
+      } else if (response.menu === "Update an employee role") {
+        employeeRoleUpdate();
+      } else {
+        // db.end();
+        console.log("Thank you for choosing our service");
+      }
+    });
 };
 
 // var to show current information in sql tables
 const viewDepartments = () => {
-    db.query(`SELECT * FROM department`, function (err, results) {
-         console.log(err);
-        console.table(results);
-        mainMenu();
-      });
-}
+  db.query(`SELECT * FROM department`, function (err, results) {
+    console.log(err);
+    console.table(results);
+    mainMenu();
+  });
+};
 
 const viewRoles = () => {
-    db.query(`SELECT * FROM roles`, function (err, results) {
-        // console.log(results);
-        console.table(results);
-        mainMenu();
-      });
-}
+  db.query(`SELECT * FROM roles`, function (err, results) {
+    // console.log(results);
+    console.table(results);
+    mainMenu();
+  });
+};
 
 const viewEmployees = () => {
-    db.query(`SELECT * FROM employees`, function (err, results) {
-        // console.log(results);
-        console.table(results);
-        mainMenu();
-      });
-}
+  db.query(`SELECT * FROM employees`, function (err, results) {
+    // console.log(results);
+    console.table(results);
+    mainMenu();
+  });
+};
 // add employee to table
 const addNewEmployee = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the employee's first name?",
         name: "first_name",
-    },
-    {
+      },
+      {
         type: "input",
         message: "What is the employee's last name?",
         name: "last_name",
-    },
-    {
+      },
+      {
         type: "input",
-        message: "What is the employee's role ID?",
+        message: "What is the role ID for the employee?",
         name: "role_id",
-    },
-    {
+      },
+      {
         type: "input",
-        message: "Is the new employee a manager? If so, enter manager ID. Otherwise, enter 0",
-        // validate: (response) => {
-        //     if(response === "Yes") {
-        //         return true
-        //     } else return false
-        // },
-        name: "manager_id", 
-    },
-]).then((response) => {
-    db.query('INSERT INTO employees SET ?', {
+        message:
+          "If employee has a manager, what is the manager ID? If none, insert null.",
+        name: "manager_id",
+      },
+
+      // {
+      //     type: "confirm",
+      //     message: "Is the new employee a manager?",
+      //     name: "manager_id",
+      // },
+    ])
+    .then((response) => {
+      var managerID = [];
+      if (response.manager_id === "null") {
+        managerID = null;
+      } else {
+        managerID = response.manager_id;
+      }
+      db.query("INSERT INTO employees SET ?", {
         first_name: response.first_name,
         last_name: response.last_name,
         role_id: response.role_id,
-        manager_id: response.manager_id,
-    })
-    viewEmployees()
-})
-}
+        manager_id: managerID,
+        // manager_id: response.manager_id|null,
+      });
+      viewEmployees();
+    });
+};
 // add a new role
 const addNewRole = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the name of the new role? ",
         name: "title",
-    },
-    {
+      },
+      {
         type: "input",
         message: "What is the salary for the new role? ",
         name: "salary",
-    },
-    {
+      },
+      {
         type: "input",
         message: "What is the department ID for the new role? ",
         name: "department_id",
-    },
-]).then((response) => {
-    db.query('INSERT INTO roles set ?', {
+      },
+    ])
+    .then((response) => {
+      db.query("INSERT INTO roles set ?", {
         title: response.title,
         salary: response.salary,
         department_id: response.department_id,
-    })
-    viewRoles()
-})
-}
+      });
+      viewRoles();
+    });
+};
 
 // adding new department
 const addNewDepartment = () => {
-    inquirer.prompt([{
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the name of the new department? ",
         name: "department_name",
-    },
-]).then((response) => {
-    db.query('INSERT INTO department set ?', {
+      },
+    ])
+    .then((response) => {
+      db.query("INSERT INTO department set ?", {
         department_name: response.department_name,
-    })
-    viewDepartments()
-})
-}
+      });
+      viewDepartments();
+    });
+};
 
 mainMenu();
 
 module.exports = db;
-
-
